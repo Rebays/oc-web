20ed9f90219259352af71bb348f5e6e0326729ec
b-tupiti <tupitibrandon@gmail.com>
2026-01-13 16:27:39 +1100

    Update site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Replace 'your-repo-name' with your actual repository name
+  basePath: '/oc-web', 
+  // This ensures CSS/JS/Images are prefixed correctly
+  assetPrefix: '/oc-web/', 
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
